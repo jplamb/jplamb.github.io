1d508dda60ade86d0285d1b077525bfd9fa6951f
John Lamb <johnlamb@Johns-MBP.attlocal.net>
2023-05-01 15:45:09 -0500

    content test

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          John Lamb
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div>
+            <p>
+              Hi, I'm a software engineer based in Austin, TX with experience writing full stack
+              applications. I most recently worked at Indeed.com in their internal Incubator. It's a group dedicated
+              to new product innovation and each product is treated like a startup.
+            </p>
+            <p>
+              In March 2023, Indeed announced layoffs affecting 15% of their workforce, including me.
+              While this was disappointing news, it's reassuring to know it was done to right-size the business
+              and not for performance reasons.
+            </p>
+            <p>
+              In the meantime, I'm taking a writing course called Write of Passage. It's a 5 week course that
+              teaches writing as a method to improve one's thinking. I hope this sharpens my communication skills
+              and helps me connect with others. You can find a link to my Substack above.
+            </p>
+            <p>
+              I'm current looking for a role as a tech lead or engineering manager. If you're hiring,&nbsp;
+               <a href="mailto:j.lamb13@gmail.com">please reach out</a>!
+            </p>
+        </div>
+      </body>
     </div>
   );
 }
